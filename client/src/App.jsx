b5acab8f254b0b86ee68153829e57e053b5f7998
Grayson <grayson.king065@gmail.com>
2024-03-04 17:37:55 -0800

    move components into own files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Habits from './Components/Habits';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -25,35 +28,7 @@ function App() {
     },
   ]
 
-  function Header(props) {
-    return(
-      <header>
-        <h1>{props.name}</h1>
-      </header>
-    )
-  }
 
-  function Habits(props) {
-    console.log(props.habits);
-    return (
-      <section>
-        <ul>
-          {props.habits.map((habit) => (
-            <li key={habit.id}>{habit.name}</li>
-          ))}
-        </ul>
-      </section>
-    );
-  }
-  
-
-  function Footer(props) {
-    return (
-      <footer>
-        <p>Copyright {props.year}</p>
-      </footer>
-    )
-  }
   return (
     <>
       <Header name="Habbit Rabbit"/>
---
+++ client/src/Components/Habits.jsx
@@ -0,0 +1,14 @@
+function Habits(props) {
+    console.log(props.habits);
+    return (
+      <section>
+        <ul>
+          {props.habits.map((habit) => (
+            <li key={habit.id}>{habit.name}</li>
+          ))}
+        </ul>
+      </section>
+    );
+  }
+
+export default Habits
